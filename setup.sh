02f4733772b8718736770258079efb12343cafbe
Viacheslav Vasilyev <avoidik@gmail.com>
2025-09-04 14:12:34 +0100

    Update cilium chart version

--- setup.sh
+++ setup.sh
@@ -24,15 +24,16 @@ kubectl wait --for condition=established crd grpcroutes.gateway.networking.k8s.i
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.3.0/config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml
 kubectl wait --for condition=established crd tlsroutes.gateway.networking.k8s.io --timeout=90s
 
-CILIUM_CHART_VERSION='1.17.5'
+CILIUM_CHART_VERSION='1.17.7'
 
-helm repo add cilium https://helm.cilium.io/
+helm repo add cilium https://helm.cilium.io/ --force-update
 helm repo update cilium
 helm upgrade \
     --install cilium cilium/cilium \
     --version "$CILIUM_CHART_VERSION" \
     --namespace kube-system \
-    --values assets/cilium-values.yaml
+    --values assets/cilium-values.yaml \
+    --atomic
 
 kubectl rollout status daemonset -n kube-system cilium --timeout=90s
 kubectl wait deployment -n kube-system cilium-operator --for condition=Available=True --timeout=90s
@@ -42,7 +43,7 @@ kubectl apply -f assets/cilium-l2.yaml
 
 METRICS_SERVER_CHART_VERSION='3.12.2'
 
-helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
+helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/ --force-update
 helm repo update metrics-server
 helm upgrade \
     --install metrics-server metrics-server/metrics-server \
